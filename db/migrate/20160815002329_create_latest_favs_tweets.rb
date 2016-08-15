a2f8104d14e5acfed202e18f5cf95ff32fd5a488
a-know <a.know.3373@gmail.com>
2016-08-15 19:17:21 +0900

    $ bundle exec rake db:migrate

--- db/migrate/20160815002329_create_latest_favs_tweets.rb
+++ db/migrate/20160815002329_create_latest_favs_tweets.rb
@@ -1,4 +1,8 @@
-class CreateLatestFavsTweets < ActiveRecord::Migration
+class CreateLatestFavsTweets < ActiveRecord::Migration[4.2]
   def change
+    create_table :latest_favs_tweets do |t|
+      t.integer :status_id
+      t.timestamps
+    end
   end
 end
